3df9534218ab6452ed9a7c7d62483069450bf033
UfnCod3r <ufnhggi_f@yahoo.com>
2013-10-22 00:05:58 -0700

    0x2

--- Eng2D_01/Engine/XList.h
+++ Eng2D_01/Engine/XList.h
@@ -127,4 +127,144 @@ namespace X
 			return iter;
 		}
 	};
+
+
+
+	//////////////////////////////////////////////////////////////////////////ForwardList, Thread Safe
+	template < typename T> class ForwardListTS
+	{
+
+	public:
+		class Node
+		{
+			friend class ForwardListTS;
+			Node(const T& value) : m_next(nullptr), m_value(value) {}
+			Node* 	m_next;
+		public:
+			T		m_value;
+			Node* next() const { return m_next; }
+
+			inline void* operator new (size_t x)  { return gGenericAllocator.alloc(x);	}
+			inline void operator delete (void* x) { gGenericAllocator.free(x);			}
+		};
+
+	private:
+		volatile uintptr	m_nodeHead;
+		Node*				m_pLastNode;
+		mutable TAC			m_tac;
+
+	public:
+		ForwardListTS()
+		{
+			m_nodeHead = 0;
+			m_pLastNode = (Node*)&m_nodeHead;
+			m_tac.init();
+		}
+		ForwardListTS(const ForwardListTS& copy)
+		{
+			m_tac.init();
+			m_nodeHead = 0;
+			m_pLastNode = (Node*)&m_nodeHead;
+			Node* node = copy.firstNode();
+			while (node)
+			{
+				push(node->m_value);
+				node = node->next();
+			}
+		}
+		~ForwardListTS()
+		{
+			clear();
+			m_tac.release();
+		}
+
+		ForwardListTS& operator = (const ForwardListTS& copy) 
+		{
+			clear();
+			Node* node = copy.firstNode();
+			while (node)
+			{
+				push(node->m_value);
+				node = node->next();
+			}
+			return *this;
+		}
+
+
+		Node*	firstNode() const		{ return (Node*)m_nodeHead;	}
+		uint	nodeCount() const
+		{
+			uint n = 0;
+			Node* iter = firstNode();
+			m_tac.lock();
+			while(iter)
+			{
+				iter = iter->next();
+				n++;
+			}
+			m_tac.unlock();
+			return n;
+		}
+
+		void clear()
+		{
+			m_tac.lock();
+			Node* iter = firstNode();
+			m_nodeHead = 0;
+			m_pLastNode = (Node*)&m_nodeHead;
+			m_tac.unlock();
+
+			while (iter)
+			{
+				Node* tmp = iter;
+				iter = iter->next();
+				delete tmp;
+			}
+			
+		}
+		Node* push(const T& value)
+		{
+			m_tac.lock();
+			Node* newNode = new Node(value);
+			m_pLastNode->m_next = newNode;
+			m_pLastNode = newNode;
+			m_tac.unlock();
+			return newNode;
+		}
+		bool deleteNode(Node* node)
+		{
+			if(node == nullptr) return false;
+			m_tac.lock();
+			Node* iter = (Node*)m_nodeHead;
+			Node* iterPre = (Node*)&m_nodeHead;
+			while (iter)
+			{
+				if(iter == node)
+				{
+					if(iter == m_pLastNode)
+						m_pLastNode = iterPre;
+					iterPre->m_next = iter->m_next;
+					delete iter;
+					return true;
+				}
+
+				iterPre = iter;
+				iter = iter->next();
+			}
+			m_tac.unlock();
+			return false;
+		}
+		Node* operator [] (size_t i) const
+		{
+			m_tac.lock();
+			Node* iter = firstNode();
+			while (((uintptr)iter) * i)
+			{
+				iter = iter->next();
+				i--;
+			}
+			m_tac.unlock();
+			return iter;
+		}
+	};
 }
